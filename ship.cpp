601b77a503b2aeee0fcf7d5491ee14c822ab340f
Erik Boyle <erikboyle@me.com>
2014-02-12 17:20:09 -0600

    Added functionality to detect sunk ships

--- ship.cpp
+++ ship.cpp
@@ -2,6 +2,8 @@
  * 
  */
 #include "ship.h"
+#include <iostream>
+using namespace std;
 
 Ship::Ship(ShipType type, char id) {
     length = type;
@@ -9,6 +11,8 @@ Ship::Ship(ShipType type, char id) {
     vertical = false;
     shipData.resize(type);
     sternRow = sternCol = 0;
+    numHits = 0;
+    sunk = false;
 }
 
 
@@ -29,4 +33,51 @@ void Ship::setShipData(bool isVertical, unsigned int row, unsigned int col) {
         }
         it->beenHit = false;
     }
+}
+
+//Searches the ships cells for row, col and marks as hit
+bool Ship::markAsHit(int row, int col) {
+    std::vector<ShipData>::iterator it;
+    for (it = shipData.begin(); it != shipData.end(); ++it) {
+        if (it->row == row && it->col == col) {
+            //mark hit and check if ship was sunk
+            //If sunk, set sunk=true and display message
+            it->beenHit = true;
+            numHits++;
+            hasBeenSunk();
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Ship::hasBeenSunk() {
+    if (numHits != length) {
+        return false;
+    }
+    switch(length) {
+        case 5:
+            cout << "Carrier(5): ";
+            break;
+        case 4:
+            cout << "Battleship(4): ";
+            break;
+        case 3:
+            cout << "Destroyer(3): ";
+            break;
+        case 2:
+            cout << "Patrol(2): ";
+            break;
+        case 1:
+            cout << "Ore(1): ";
+            break;
+        default:
+            cout << "Error in Ship::hasBeenSunk()\n";
+            exit(-1);
+            break;
+    }
+    cout << "has been sunk!\n";
+
+   sunk = true;
+   return true;
 }
--- ship.h
+++ ship.h
@@ -1,9 +1,10 @@
 /* TODO:
- * -mark ship as hit
+ * -better way to find ship that was hit?
  */
 #ifndef SHIP_H
 #define SHIP_H
 #include <vector>
+#include <iostream>
 
 enum ShipType {
     CARRIER     = 5, 
@@ -30,15 +31,24 @@ public:
     int getLength()         { return length; }
     bool isVertical()       { return vertical; }
     char getID()            { return ID; }
+    bool isSunk()           { return sunk; }
     
     //Assigns each cell of the ship (ShipData vector) to a board cell and marks as not hit
     void setShipData(bool isVertical, unsigned int sternRow, unsigned int sternCol);
 
+    //Marks the ship as hit if it sits on row, col
+    bool markAsHit(int row, int col); 
+    
+    bool hasBeenSunk();
+
 private:
     char ID;
     unsigned int length;
+    unsigned int numHits;
+    bool sunk;
     bool vertical;
     int sternRow, sternCol;
+
 
     //a ship is a vector of ShipData
     std::vector<ShipData> shipData;
